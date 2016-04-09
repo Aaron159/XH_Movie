b3338479a5eaee02ad1ddb074545a06bda2a117f
mac51 <mac51@gg.local>
2016-04-09 14:32:09 +0800

    Initial Commit

---
+++ 项目一/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  项目一
+//
+//  Created by mac51 on 16/4/9.
+//  Copyright © 2016年 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
